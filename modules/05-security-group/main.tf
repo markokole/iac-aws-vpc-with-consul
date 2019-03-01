efdff257e7bee3e515bc5eb2b8c7a8dfb5ac532c
Marko Kljajic <marko_kole@yahoo.com>
2019-03-01 12:06:45 +0100

    Update main.tf

--- modules/05-security-group/main.tf
+++ modules/05-security-group/main.tf
@@ -6,11 +6,7 @@ locals {
 
 }
 
-provider "aws" {
-  region = "${local.region}"
-}
-
-resource "aws_security_group_rule" "port_22_to_world" {
+resource "aws_security_group" "port_22_to_world" {
   type        = "ingress"
   description = "SSH to world"
   from_port   = "22"
@@ -19,24 +15,3 @@ resource "aws_security_group_rule" "port_22_to_world" {
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = "${local.default_security_group_id}"
 }
-/*
-resource "aws_security_group_rule" "port_8080_to_world" {
-  type        = "ingress"
-  description = "Ambari to world"
-  from_port   = "8080"
-  to_port     = "8080"
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "${local.default_security_group_id}"
-}
-
-resource "aws_security_group_rule" "port_6080_to_world" {
-  type        = "ingress"
-  description = "Ranger to world"
-  from_port   = "6080"
-  to_port     = "6080"
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = "${local.default_security_group_id}"
-}
-*/
