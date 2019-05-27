cd2f474923db923ea9ae0cd4c5ce9b718cf869e1
Marko Kljajic <marko_kole@yahoo.com>
2019-05-27 16:12:32 +0200

    typo

--- modules/05-security-group/write_to_consul.tf
+++ modules/05-security-group/write_to_consul.tf
@@ -2,7 +2,7 @@ resource "consul_keys" "app" {
   datacenter = "${local.datacenter}"
 
   key {
-    path = "${local.path_to_generated_aws_properties}port_22_to_world"
+    path = "${local.path_to_generated_aws_properties}/port_22_to_world"
     value = "${aws_security_group_rule.port_22_to_world.id}"
   }
 }
